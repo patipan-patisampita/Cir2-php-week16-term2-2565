e32e61a6fbc44ac8b8ae7962d8171a623a933c01
patipan-patisampita <patipan.patisampita@gmail.com>
2023-02-02 16:41:27 +0700

    update

--- admin/index.php
+++ admin/index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>สมาชิกหรือลูกค้า</title>
+    <title>ผู้ดูแลระบบ</title>
 </head>
 
 <body>
-    <h1>สมาชิกหรือลูกค้า</h1>
+    <h1>ผู้ดูแลระบบ</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad atque aliquam a nobis dolorem enim, incidunt excepturi rerum! Voluptas cupiditate rem dicta dolores aliquid ea.</p>
 </body>
 
