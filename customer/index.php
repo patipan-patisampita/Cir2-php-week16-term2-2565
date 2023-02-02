44b9ae9f4c37abf2fd85d9b4b73781ac9a56dfb6
patipan-patisampita <patipan.patisampita@gmail.com>
2023-02-02 16:33:34 +0700

    update

--- customer/index.php
+++ customer/index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>สมาชิกหรือลูกค้า</title>
+    <title>ผู้ดูแลร้านอาหาร</title>
 </head>
 
 <body>
-    <h1>สมาชิกหรือลูกค้า</h1>
+    <h1>ผู้ดูแลร้านอาหาร</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad atque aliquam a nobis dolorem enim, incidunt excepturi rerum! Voluptas cupiditate rem dicta dolores aliquid ea.</p>
 </body>
 
