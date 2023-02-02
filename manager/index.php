32720a0c53ecc8f7330f2500563821298bf58488
patipan-patisampita <patipan.patisampita@gmail.com>
2023-02-02 16:31:57 +0700

    update

---
+++ manager/index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ผู้ดูแลร้านอาหาร</title>
+</head>
+
+<body>
+    <h1>ผู้ดูแลร้านอาหาร</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad atque aliquam a nobis dolorem enim, incidunt excepturi rerum! Voluptas cupiditate rem dicta dolores aliquid ea.</p>
+</body>
+
+</html>
